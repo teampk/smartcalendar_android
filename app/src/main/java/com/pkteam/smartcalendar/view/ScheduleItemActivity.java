e83ad9ff8e372d3e29349f5384f9f65997adff33
nuggy875 <jinugpaeng@gmail.com>
2019-03-04 15:08:47 +0900

    scheduling UI

--- app/src/main/java/com/pkteam/smartcalendar/view/ScheduleItemActivity.java
+++ app/src/main/java/com/pkteam/smartcalendar/view/ScheduleItemActivity.java
@@ -3,6 +3,7 @@ package com.pkteam.smartcalendar.view;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.pkteam.smartcalendar.R;
@@ -18,8 +19,15 @@ public class ScheduleItemActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_schedule_item);
+        binding.setScheduling(this);
 
+    }
 
+    public void finishView(View view){
+        finish();
+    }
 
+    public void onClickFooter(View view){
+        finish();
     }
 }
